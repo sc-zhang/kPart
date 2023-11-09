1ba9a1fae61d798eb542529851475fb0cbe71f17
sc-zhang <zsc-zhang@foxmail.com>
2023-11-09 12:12:57 +0800

    allow more than 2 ancestors

--- kpart/classify_result.py
+++ kpart/classify_result.py
@@ -4,37 +4,41 @@ from utils import time_print
 
 
 def classify_result(in_csv, out_csv):
-	time_print("Starting classify")
-	with open(in_csv, 'r') as fin:
-		with open(out_csv, 'w') as fout:
-			for line in fin:
-				data = line.strip().split(',')
-				if data[0] == 'ContigName':
-					sample_list = []
-					for info in data[-2:]:
-						sample_list.append(info.split()[-1])
-					data.append("Classification")
-					fout.write("%s\n"%(','.join(data)))
-				else:
-					ctg = data[0]
-					vals = list(map(int, data[3:]))
-					for i in range(0, len(vals)):
-						if vals[i] == 0:
-							vals[i] = 1
-					if vals[0]*1.0/vals[1] > 2.0:
-						data.append(sample_list[0])
-					elif vals[1]*1.0/vals[0] > 2.0:
-						data.append(sample_list[1])
-					else:
-						data.append('Undetermined')
-					fout.write("%s\n"%(','.join(data)))
-	time_print("Finished")
+    time_print("Starting classify")
+    with open(in_csv, 'r') as fin:
+        with open(out_csv, 'w') as fout:
+            for line in fin:
+                data = line.strip().split(',')
+                if data[0] == 'ContigName':
+                    sample_list = []
+                    for info in data[3:]:
+                        sample_list.append(info.split()[-1])
+                    data.append("Classification")
+                    fout.write("%s\n" % (','.join(data)))
+                else:
+                    vals = list(map(int, data[3:]))
+                    max_smp = ["", 0]
+                    sec_max_smp = ["", 0]
+                    for _ in range(len(vals)):
+                        if vals[_] > max_smp[1]:
+                            sec_max_smp[0] = max_smp[0]
+                            sec_max_smp[1] = max_smp[1]
+                            max_smp[0] = sample_list[_ - 3]
+                            max_smp[1] = vals[_]
+                        elif vals[_] > sec_max_smp[1]:
+                            sec_max_smp[0] = sample_list[_ - 3]
+                            sec_max_smp[1] = vals[_]
+                    if max_smp[1] > sec_max_smp[1] * 2.:
+                        data.append(max_smp[0])
+                    else:
+                        data.append('Undetermined')
+                    fout.write("%s\n" % (','.join(data)))
+    time_print("Finished")
 
 
 if __name__ == "__main__":
-	if len(sys.argv) < 3:
-		print("Usage: python %s <in_csv> <out_csv>"%sys.argv[0])
-	else:
-		in_csv, out_csv = sys.argv[1:]
-		classify_result(in_csv, out_csv)
-
+    if len(sys.argv) < 3:
+        print("Usage: python %s <in_csv> <out_csv>" % sys.argv[0])
+    else:
+        in_csv, out_csv = sys.argv[1:]
+        classify_result(in_csv, out_csv)
--- kpart/stat_kmer_dist.py
+++ kpart/stat_kmer_dist.py
@@ -6,96 +6,95 @@ from utils import time_print
 
 
 def sub_stat(ref_fn, ctg, qry_dbs):
-	stat_db = {'common': 0}
-	qry_db = {}
-	time_print("\tDealing %s"%ctg)
-	for qry_n in qry_dbs:
-		stat_db[qry_n] = 0
-		if ctg in qry_dbs[qry_n]:
-			idx, fn = qry_dbs[qry_n][ctg]
-			with open(fn, 'r') as fin:
-				for line in fin:
-					kmer = line.strip().split()[0]
-					if kmer not in qry_db:
-						qry_db[kmer] = qry_n
-					else:
-						qry_db[kmer] = 'common'
-	
-	kmer_cnt = 0
-	with open(ref_fn, 'r') as fin:
-		for line in fin:
-			kmer = line.strip().split()[0]
-			kmer_cnt += 1
-			if kmer in qry_db:
-				qry_n = qry_db[kmer]
-				stat_db[qry_n] += 1
-	
-	return ctg, stat_db, kmer_cnt
+    stat_db = {'common': 0}
+    qry_db = {}
+    time_print("\tDealing %s" % ctg)
+    for qry_n in qry_dbs:
+        stat_db[qry_n] = 0
+        if ctg in qry_dbs[qry_n]:
+            idx, fn = qry_dbs[qry_n][ctg]
+            with open(fn, 'r') as fin:
+                for line in fin:
+                    kmer = line.strip().split()[0]
+                    if kmer not in qry_db:
+                        qry_db[kmer] = qry_n
+                    else:
+                        qry_db[kmer] = 'common'
+
+    kmer_cnt = 0
+    with open(ref_fn, 'r') as fin:
+        for line in fin:
+            kmer = line.strip().split()[0]
+            kmer_cnt += 1
+            if kmer in qry_db:
+                qry_n = qry_db[kmer]
+                stat_db[qry_n] += 1
+
+    return ctg, stat_db, kmer_cnt
 
 
 def stat_kmer_dist(in_ref_dir, in_qry_dirs, out_stat, ts):
-	time_print("Loading query file lists")
-	qry_dbs = {}
-	for i in range(0, len(in_qry_dirs)):
-		in_qry_dir = in_qry_dirs[i]
-		qry_n = in_qry_dir.split('/')[-1]
-		qry_dbs[qry_n] = {}
-		for fn in os.listdir(in_qry_dir):
-			ctg = '.'.join(fn.split('.')[:-1])
-			qry_dbs[qry_n][ctg] = [i, os.path.join(in_qry_dir, fn)]
-	
-	time_print("Statistic")
-	pool = multiprocessing.Pool(processes=ts)
-	res = []
-	for fn in os.listdir(in_ref_dir):
-		ref_fn = os.path.join(in_ref_dir, fn)
-		ctg = '.'.join(fn.split('.')[:-1])
-		r = pool.apply_async(sub_stat, (ref_fn, ctg, qry_dbs,))
-		res.append(r)
-	pool.close()
-	pool.join()
+    time_print("Loading query file lists")
+    qry_dbs = {}
+    for i in range(0, len(in_qry_dirs)):
+        in_qry_dir = in_qry_dirs[i]
+        qry_n = in_qry_dir.split('/')[-1]
+        qry_dbs[qry_n] = {}
+        for fn in os.listdir(in_qry_dir):
+            ctg = '.'.join(fn.split('.')[:-1])
+            qry_dbs[qry_n][ctg] = [i, os.path.join(in_qry_dir, fn)]
 
-	total_db = {}
-	qry_ns = {}
-	for r in res:
-		ctg, stat_db, kmer_cnt = r.get()
-		total_db[ctg] = {'cnt': kmer_cnt}
-		for qry_n in stat_db:
-			if qry_n not in qry_ns:
-				qry_ns[qry_n] = 1
-			total_db[ctg][qry_n] = stat_db[qry_n]
+    time_print("Statistic")
+    pool = multiprocessing.Pool(processes=ts)
+    res = []
+    for fn in os.listdir(in_ref_dir):
+        ref_fn = os.path.join(in_ref_dir, fn)
+        ctg = '.'.join(fn.split('.')[:-1])
+        r = pool.apply_async(sub_stat, (ref_fn, ctg, qry_dbs,))
+        res.append(r)
+    pool.close()
+    pool.join()
 
-	qry_list = []
-	for qry_n in qry_ns:
-		if qry_n == 'common':
-			continue
-		qry_list.append(qry_n)
-	qry_list = sorted(qry_list)
-	time_print("Writing result")
-	with open(out_stat, 'w') as fout:
-		fout.write("ContigName,No. of Kmers,")
-		fout.write("\"No. of kmers that are shared by %s\","%(','.join(qry_list)))
-		tmp_list = []
-		for qry_n in qry_list:
-			tmp_list.append("No. of kmers that are present only in %s"%qry_n)
-		fout.write(','.join(tmp_list))
-		fout.write("\n")
-		for ctg in sorted(total_db):
-			fout.write("%s,%d,%d,"%(ctg, total_db[ctg]['cnt'], total_db[ctg]['common']))
-			tmp_list = []
-			for qry_n in qry_list:
-				tmp_list.append(str(total_db[ctg][qry_n]))
-			fout.write(','.join(tmp_list))
-			fout.write("\n")
+    total_db = {}
+    qry_ns = {}
+    for r in res:
+        ctg, stat_db, kmer_cnt = r.get()
+        total_db[ctg] = {'cnt': kmer_cnt}
+        for qry_n in stat_db:
+            if qry_n not in qry_ns:
+                qry_ns[qry_n] = 1
+            total_db[ctg][qry_n] = stat_db[qry_n]
 
-	time_print("Finished")
+    qry_list = []
+    for qry_n in qry_ns:
+        if qry_n == 'common':
+            continue
+        qry_list.append(qry_n)
+    qry_list = sorted(qry_list)
+    time_print("Writing result")
+    with open(out_stat, 'w') as fout:
+        fout.write("ContigName,No. of Kmers,")
+        fout.write("\"No. of shared kmers\",")
+        tmp_list = []
+        for qry_n in qry_list:
+            tmp_list.append("No. of kmers that are present only in %s" % qry_n)
+        fout.write(','.join(tmp_list))
+        fout.write("\n")
+        for ctg in sorted(total_db):
+            fout.write("%s,%d,%d," % (ctg, total_db[ctg]['cnt'], total_db[ctg]['common']))
+            tmp_list = []
+            for qry_n in qry_list:
+                tmp_list.append(str(total_db[ctg][qry_n]))
+            fout.write(','.join(tmp_list))
+            fout.write("\n")
+
+    time_print("Finished")
 
 
 if __name__ == "__main__":
-	if len(sys.argv) < 5:
-		print("Usage: python %s <in_ref_dir> <in_qry_dirs> <out_stat> <threads>"%sys.argv[0])
-	else:
-		in_ref_dir, in_qry_dirs, out_stat, ts = sys.argv[1:]
-		in_qry_dirs = in_qry_dirs.split(',')
-		stat_kmer_dist(in_ref_dir, in_qry_dirs, out_stat, int(ts))
-
+    if len(sys.argv) < 5:
+        print("Usage: python %s <in_ref_dir> <in_qry_dirs> <out_stat> <threads>" % sys.argv[0])
+    else:
+        in_ref_dir, in_qry_dirs, out_stat, ts = sys.argv[1:]
+        in_qry_dirs = in_qry_dirs.split(',')
+        stat_kmer_dist(in_ref_dir, in_qry_dirs, out_stat, int(ts))
